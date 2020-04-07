94fbf2a47cc530250bf93d9cba98634ab3e18750
vikramkrishna2000 <vikramkk2000@gmail.com>
2020-04-07 16:19:19 +0530

    Add form so that we can save a new todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -20,15 +20,19 @@ class TodosController < ApplicationController
         todo_text = params[:todo_text]
         due_date = DateTime.parse(params[:due_date])
         new_todo = Todo.create!(todo_text: todo_text, due_date:due_date,completed:false)
-        response_text="Hey , your new todo is created with the id #{new_todo.id}"
-        render plain: response_text
+
+        redirect_to todos_path
+    
     end
+
     def update
         id = params[:id]
         completed = params[:completed]      
         todo = Todo.find(id)
         todo.completed = completed
         todo.save!
+
         render plain: "Updated todo completed status to #{completed}"
+    
     end    
 end
