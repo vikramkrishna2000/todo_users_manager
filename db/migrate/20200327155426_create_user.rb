1c1d2551ae7a64d0a0d3b903f095e5918978bb25
vikramkrishna2000 <vikramkk2000@gmail.com>
2020-04-12 20:51:36 +0530

    Added a migration to create a new users table

--- db/migrate/20200327155426_create_user.rb
+++ db/migrate/20200327155426_create_user.rb
@@ -1,9 +1,11 @@
 class CreateUser < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.string :email
-      t.string :password
+      t.string :password_digest
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,12 @@ ActiveRecord::Schema.define(version: 2020_04_04_142103) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
